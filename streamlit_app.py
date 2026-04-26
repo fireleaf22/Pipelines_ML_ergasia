a8b8e69241fa19c93342b2515bdc991d61b77c8a
fireleaf22 <45073100+fireleaf22@users.noreply.github.com>
2026-04-26 20:41:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('ML App')
 
 st.info('This is the App for the project')
+
+df = pd.read_csv('https://raw.githubusercontent.com/fireleaf22/Pipelines_ML_ergasia/refs/heads/master/Video_Games_Sales_Cleaned.csv')
+df
