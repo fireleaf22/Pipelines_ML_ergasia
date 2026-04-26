723e05a46063d6d63ac0825737655892df08f478
fireleaf22 <45073100+fireleaf22@users.noreply.github.com>
2026-04-26 23:30:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,17 @@ st.info('This is the App for the project')
 
 with st.expander('Data'):
   st.write('**Raw Dataset**')
-  df = pd.read_csv('https://raw.githubusercontent.com/fireleaf22/Pipelines_ML_ergasia/refs/heads/master/reels_attention_span_dataset_12000.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/fireleaf22/Pipelines_ML_ergasia/refs/heads/master/student-por.csv')
   df
 
 
 with st.expander('Chart'):
-  st.area_chart(df, x="task_completion_rate", y="reels_watch_time_hours", color="stress_level")
+  st.scatter_chart(
+    df,
+    x="studytime",
+    y="G3",
+    color="schoolsup",
+    size="sex",
+)
   
 
