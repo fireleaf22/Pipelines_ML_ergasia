77283ef96978e6507521dab089bd11ade7db52ed
fireleaf22 <45073100+fireleaf22@users.noreply.github.com>
2026-04-26 22:24:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.info('This is the App for the project')
 
 with st.expander('Data'):
   st.write('**Raw Dataset**')
-  df = pd.read_csv('https://raw.githubusercontent.com/fireleaf22/Pipelines_ML_ergasia/refs/heads/master/Video_Games_Sales_Cleaned.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/fireleaf22/Pipelines_ML_ergasia/refs/heads/master/Video_Games_Sales_as_at_22_Dec_2016.csv')
   df
 
 
@@ -16,7 +16,11 @@ with st.expander('Chart'):
   #st.bar_chart(df, x="critic_score", y="total_sales", color="genre", horizontal=True)
   st.scatter_chart(
     df,
-    x="genre",
-    y="critic_score",
-    color="total_sales",
+    x="Global_Sales",
+    y="Critic_Score",
+    color="Genre",
   )
+
+#Data preperations
+#with st.
+
